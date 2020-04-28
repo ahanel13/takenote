dd040e5c782ccdda9227133df62f1be9dae10961
Anthony Hanel <ahanel13@gmail.com>
2020-04-28 16:48:37 -0500

    updated root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
   resources :users
   resources :courses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
