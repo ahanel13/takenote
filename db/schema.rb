47fccbdf511fe1c4e3b3dd9f7da87134e35e5323
Anthony Hanel <ahanel13@gmail.com>
2020-04-28 23:00:54 -0500

    added devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -67,9 +67,7 @@ ActiveRecord::Schema.define(version: 2020_04_28_210236) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
     t.string "school"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
