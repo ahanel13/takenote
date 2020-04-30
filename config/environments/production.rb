a2cd67714d1df1d5c10feb3b43efe84be156ebbe
Anthony Hanel <ahanel13@gmail.com>
2020-04-29 21:27:13 -0500

    adding smpt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,6 +111,6 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
 
-  config.action_mailer.delivery_method = 
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "takenote-tecm4400.herokuapp.com" }
 end
