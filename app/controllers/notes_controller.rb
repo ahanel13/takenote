620346da8cc8ba20d0dc8a7f8f8a9699ad30069a
Anthony Hanel <ahanel13@gmail.com>
2020-05-02 10:42:44 -0700

    Updating controllers to require authenitification

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < ApplicationController
   before_action :set_note, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :update, :destroy]
 
   # GET /notes
   # GET /notes.json
@@ -13,7 +14,6 @@ class NotesController < ApplicationController
   end
 
   # GET /notes/new
-  before_action :authenticate_user!
   def new
     @note = Note.new
     @note.users_id = current_user.id
