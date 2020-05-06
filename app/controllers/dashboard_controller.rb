33a8053e8a5cb72e2a416eae842c19edb60fcde2
Anthony Hanel <ahanel13@gmail.com>
2020-05-06 15:24:16 -0500

    Displaying notes created by the user in the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
+    @user_notes = current_user.notes
+    @save_notes
+    @user_courses
   end
 end
